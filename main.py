54377e918e242221e84a1e5e57afd4a83a9015a5
François <francois.fedian.4012@free.fr>
2025-04-06 15:15:10 +0200

    Ajout des autres fichiers

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 import json
-import yaml
 import os
+import sys
+import yaml
 import toml
 
 def guess_file_language(file):
     """Devine le langage du fichier à partir de son extension"""
     
-    filename_splitted = os.path.split(file)
-    print(filename_splitted)
+    extension = os.path.splitext(file)[1].lower()
+    match extension:
+        case ".json":
+            language = "json"
+        case ".yaml"|".yml":
+            language = "yaml"
+        case ".toml":
+            language = "toml"
+        case _:
+            language = "inconnu"
+    return language
 
-guess_file_language("music.json")
+def read_file_by_language(file, language):
+    with open(file, "r") as input_file:
+        match language:
+            case "json":
+                data = json.load(input_file)
+            case "yaml":
+                data = yaml.safe_load(input_file)
+            case "toml":
+                data = toml.load(input_file)
+    return data
+
+def main():
+    file = "data.yml"
+    language_of_file = guess_file_language(file)     
+    print(read_file_by_language(file, language_of_file))
+
+if __name__ == "__main__":
+    main()
